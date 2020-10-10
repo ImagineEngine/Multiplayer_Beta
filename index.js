ece6010e2e9ce02698a5021cc09a44c1ffadd4d4
Imagine Engine <58477150+ImagineEngine@users.noreply.github.com>
2020-10-10 12:38:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-fetch('https://db.imagineengine.repl.co/room2', {method: 'POST', body: JSON.stringify({'okay':1}), headers: {'Content-Type': 'application/json'}}).then(response => response.text()).then(text => console.log(text))
+fetch('https://db.imagineengine.repl.co/room2', {method: 'POST', body: JSON.stringify({'okay':1}), headers: {'Content-Type': 'application/json'}}).then(response => response.text()).then(text => document.write(text))
