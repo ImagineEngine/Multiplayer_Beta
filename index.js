cf7357a011b4011d2b6dfc523af0f5ccb922d965
Imagine Engine <58477150+ImagineEngine@users.noreply.github.com>
2020-10-10 12:29:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-require('fs')
 fetch('https://db.imagineengine.repl.co/room2', {method: 'POST', body: JSON.stringify({'okay':1}), headers: {'Content-Type': 'application/json'}}).then(response => response.text()).then(text => console.log(text))
